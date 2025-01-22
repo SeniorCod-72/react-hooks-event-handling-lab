75984ed30f03878d309425122b203d9ee4d7b78b
andrew72-max <andrew.nyongesa@student.moringaschool.com>
2025-01-22 23:26:33 +0300

    changes added

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,26 @@
-// Code EyesOnMe Component Here
+// src/components/EyesOnMe.js
+
+import React from 'react';
+
+function EyesOnMe() {
+  // Event handler for focus
+  const handleFocus = () => {
+    console.log('Good!');
+  };
+
+  // Event handler for blur
+  const handleBlur = () => {
+    console.log('Hey! Eyes on me!');
+  };
+
+  return (
+    <button 
+      onFocus={handleFocus}  // Event listener for focus
+      onBlur={handleBlur}    // Event listener for blur
+    >
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,20 @@
-// Code Keypad Component Here
+// src/components/Keypad.js
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+import React from 'react';
+
+function Keypad() {
+  // Event handler for change event
+  const handleChange = () => {
+    console.log('Entering password...');
+  };
+
+  return (
+    <input
+      type="password"  // Ensures the input is of password type (characters are hidden)
+      onChange={handleChange}  // Listens for changes to the input field
+      placeholder="Enter password"  // Optional: placeholder text for user guidance
+    />
+  );
 }
 
 export default Keypad;
